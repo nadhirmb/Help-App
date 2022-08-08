e13cc8f624fef0e9cd220050decf36cada5b7196
talout-chattah <taloutchattah2001@gmail.com>
2022-08-08 11:46:45 +0100

    just a push

---
+++ src/Colors.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+ export const ThemeColors = {
+  green: "#9DD180",
+  yellow: "#EDEB8F",
+  purple: "purple",
+  blue: "#39C3E7"
+};
--- src/components/navbar/button/HelpMeStyle.js
+++ src/components/navbar/button/HelpMeStyle.js
@@ -1,16 +1,18 @@
 import styled from 'styled-components';
+import "../../../Colors";
 
 export const ButtonStyle = styled.button`
 appearance: none;
 padding: 10px 20px ;
 font-weight: Bold;
 font-size: 16px;
-background-color: #EDEB8F ;
+background-color: #EDEB8F ; //Yellow
 color: #39C3E7 ;
 border-radius: 25px;
-border: 2px solid #4CAF50;
+border: 2px solid #39C3E7;
 transition-duration: 0.4s;
 &:hover {
-  background-color: #4CAF50; /* Green */
+  background-color: #39C3E7; /* Blue */
+  border: 2px solid #EDEB8F;
   color: white;
 }`;
